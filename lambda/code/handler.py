49f3018b653d4da97a76a987a60046ea00ac5298
dulce-brillio <dulce.cruz@brillio.com>
2023-10-04 13:37:00 -0600

    layer dependencies

--- lambda/code/handler.py
+++ lambda/code/handler.py
@@ -8,30 +8,35 @@ def handler(event, context):
     db_user = os.environ["DB_USER"]
     db_password_secret = os.environ["DB_PASSWORD_SECRET"]
 
+    try:
     # Retrive secret
-    client = boto3.client('secretsmanager')
-    response = client.get_secret_value(SecretId=db_password_secret)
-    db_password = response["SecretString"]
+        client = boto3.client('secretsmanager')
+        response = client.get_secret_value(SecretId=db_password_secret)
+        db_password = response["SecretString"]
 
-    connection = psy.connect(
-        host=db_endpoint,
-        port=db_port,
-        user=db_user,
-        db_password=db_password,
-        database="postgres"
-    )
+        connection = psy.connect(
+            host=db_endpoint,
+            port=db_port,
+            user=db_user,
+            db_password=db_password,
+            database="postgres"
+        )
 
-    cursor = connection.cursor()
+        cursor = connection.cursor()
 
-    # Create new db
-    cursor.execute("CREATE DATABASE IF NOT EXISTS my_database;")
-    cursor.execute("USE my_database;")
+        # Create new db
+        cursor.execute("CREATE DATABASE IF NOT EXISTS my_database;")
+        cursor.execute("USE my_database;")
 
-    # Create a sample table
-    cursor.execute("CREATE TABLE IF NOT EXISTS sample_table (id serial PRIMARY KEY, data varchar);")
-    cursor.execute("GRANT ALL ON sample_table TO mydbuser;")
-    connection.commit()
-    cursor.close()
-    connection.close()
+        # Create a sample table
+        cursor.execute("CREATE TABLE IF NOT EXISTS sample_table (id serial PRIMARY KEY, data varchar);")
+        cursor.execute("GRANT ALL ON sample_table TO mydbuser;")
+        connection.commit()
+        cursor.close()
+        connection.close()
 
-    return "Database and table created succesfully"
+        return "Database and table created succesfully"
+    except Exception as error:
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
+        return "error"
--- rds/aurora.py
+++ rds/aurora.py
@@ -4,9 +4,8 @@ from aws_cdk import (
     aws_secretsmanager as secretsmanager,
     aws_ec2 as ec2,
     aws_iam as iam,
-    aws_stepfunctions_tasks as tasks,
-    aws_stepfunctions as sfn,
-    Fn, Stack
+    custom_resources as cr,
+    Stack
 )
 
 from constructs import Construct
@@ -18,54 +17,17 @@ class AuroraStack(Stack):
                   ingress_sources:list=[],
                   **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
+        
         # Create a secret to store rds credentials
         rds_secret = secretsmanager.Secret(
             self, "RDSSecret",
             generate_secret_string=secretsmanager.SecretStringGenerator(
                 secret_string_template='{"username": "mydbuser"}',
                 generate_string_key="password",
-                exclude_characters=';=+[]{}"',
+                exclude_characters='/@;=+[]{}"',
             )
         )
-
-        azs = azs = Fn.get_azs()
-        #rds_vpc = ec2.Vpc.from_vpc_attributes(self, 'ExistingVPC', availability_zones=azs, vpc_id=vpc_id)
-        #rds_vpc_subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
-
-        # Create rds security group
-        dbsg = ec2.SecurityGroup(self, "DatabaseSecurityGroup",
-             vpc = rds_vpc,
-             allow_all_outbound = True,
-             description = id + " Database",
-             security_group_name = id + " Database",
-           )
         
-        allAll = ec2.Port(protocol=ec2.Protocol("ALL"), string_representation="ALL")
-        tcp5432 = ec2.Port(protocol=ec2.Protocol("TCP"), from_port=5432, to_port=5432, string_representation="tcp5432 PostgreSQL")
-        tcp1433 = ec2.Port(protocol=ec2.Protocol("TCP"), from_port=1433, to_port=1433, string_representation="tcp1433 MSSQL")
-
-        dbsg.add_ingress_rule(
-        peer =dbsg,
-        connection =allAll,
-        description="all from self"
-        )
-        dbsg.add_egress_rule(
-        peer =ec2.Peer.ipv4("0.0.0.0/0"),
-        connection =allAll,
-        description="all out"
-        )
-
-        connection_port = tcp5432
-        connection_name = "tcp5432 PostgreSQL"
-
-        for ingress_source in ingress_sources:
-            dbsg.add_ingress_rule(
-            peer =ingress_source,
-            connection =tcp1433,
-            description="tcp1433 MSSQL"
-            )
-
         # Create an Aurora Postgresql database
         database = rds.DatabaseCluster(self, "AuroraDatabase",
             engine=rds.DatabaseClusterEngine.aurora_postgres(version=rds.AuroraPostgresEngineVersion.VER_13_4),
@@ -74,46 +36,19 @@ class AuroraStack(Stack):
                 "instance_type": ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE4_GRAVITON, ec2.InstanceSize.MEDIUM),
                 "vpc": rds_vpc,
                 "vpc_subnets": ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED)
-            }
+            },
         )
 
-        lambdaLayer = _lambda.LayerVersion(self, 'lambda-layer',
-                  code = _lambda.AssetCode('lambda/layer/'),
-                  compatible_runtimes = [_lambda.Runtime.PYTHON_3_11],
-        ) 
-
-        # Create lambda function
-        rds_lambda = _lambda.Function(
-            self, "RDSLambda",
-            runtime=_lambda.Runtime.PYTHON_3_11,
-            handler="handler.handler",
-            code=_lambda.Code.from_asset("lambda/code"),
-            layers=[lambdaLayer],
-            vpc=rds_vpc,
-            security_groups=[ec2.SecurityGroup.from_security_group_id(self,"SG",dbsg.security_group_id)],
-            vpc_subnets= ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
-            environment={
-                "DB_ENDPOINT": database.cluster_endpoint.hostname,
-                "DB_PORT": str(database.cluster_endpoint.port),
-                "DB_USER": "mydbuser",
-                "DB_PASSWORD_SECRET": rds_secret.secret_name,  
-            }
+        proxy = rds.DatabaseProxy(self, "Proxy",
+            proxy_target=rds.ProxyTarget.from_cluster(database),
+            secrets=[rds_secret],
+            vpc=rds_vpc
         )
 
-
-        state_machine = sfn.StateMachine(self, "LambdaStateMachine",
-            definition= tasks.LambdaInvoke(self, "Invoke with empty object as payload",
-            lambda_function=rds_lambda,
-            )
-        )
-
-        # AMI
-        amzn_linux = ec2.MachineImage.latest_amazon_linux(
-            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-            edition=ec2.AmazonLinuxEdition.STANDARD,
-            virtualization=ec2.AmazonLinuxVirt.HVM,
-            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
-            )
+        lambda_role = iam.Role(self, "DBProxyRole", assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"))
+        lambda_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole"))
+        lambda_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaVPCAccessExecutionRole"))
+        proxy.grant_connect(lambda_role, "admin")
 
         # Instance Role and SSM Managed Policy
         role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
@@ -126,6 +61,63 @@ class AuroraStack(Stack):
             ec2.Port.tcp(5432),
             "Allow inbound from EC2"
         )
+        
+        lambdaLayer = _lambda.LayerVersion(self, 'lambda-layer',
+                  code = _lambda.AssetCode('lambda/layer/'),
+                  compatible_runtimes = [_lambda.Runtime.PYTHON_3_9],
+        )
+
+        # Create lambda function
+        rds_lambda = _lambda.Function(
+            self, "RDSLambda",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="handler.handler",
+            code=_lambda.Code.from_asset("lambda/code"),
+            layers=[lambdaLayer],
+            vpc=rds_vpc,
+            security_groups=[ec2.SecurityGroup.from_security_group_id(self,"SG",ec2_security_group.security_group_id)],
+            vpc_subnets= ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED),
+            environment={
+                "DB_ENDPOINT": database.cluster_endpoint.hostname,
+                "DB_PORT": str(database.cluster_endpoint.port),
+                "DB_USER": "mydbuser",
+                "DB_PASSWORD_SECRET": rds_secret.secret_name,  
+            },
+            role=lambda_role
+        )
+
+        rds_secret.grant_read(rds_lambda)
+
+        invoke_lambda_resource = cr.AwsCustomResource(
+            self,
+            "InvokeLambdaResource",
+            policy=cr.AwsCustomResourcePolicy.from_statements([
+                iam.PolicyStatement(
+                    effect=iam.Effect.ALLOW,
+                    actions=[
+                        "lambda:InvokeFunction"
+                    ],
+                    resources=[rds_lambda.function_arn]
+                )
+            ]),
+            on_create={
+                "service": "Lambda",
+                "action": "invoke",
+                "parameters": {
+                    "FunctionName": rds_lambda.function_name,
+                    "InvocationType": "RequestResponse",
+                },
+                "physical_resource_id": cr.PhysicalResourceId.from_response("Payload"),
+            },
+        )
+
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
 
         # Instance
         instance = ec2.Instance(self, "Instance",
@@ -136,4 +128,3 @@ class AuroraStack(Stack):
             role = role,
             security_group = ec2_security_group
             )
-
